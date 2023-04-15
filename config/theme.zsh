0242b83818cb96db82e455d6b708b5deba2b14b1
lizhi <liiil825.2012@gmail.com>
2023-04-15 11:19:57 +0800

    add theme

---
+++ config/theme.zsh
@@ -0,0 +1,8 @@
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+TERM="screen-256color"
+
+source $ZSH_CUSTOM/themes/powerlevel10k/powerlevel10k.zsh-theme
+
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
